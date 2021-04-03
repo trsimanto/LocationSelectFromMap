387bbd718195984172b4f31550d04a3e2d769e40
Towhid <simanto14feb@gmail.com>
2021-04-03 17:44:28 +0600

    show current location on map

--- app/src/main/java/com/towhid/map/MainActivity.kt
+++ app/src/main/java/com/towhid/map/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.towhid.map
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private var mMap: GoogleMap? = null
     lateinit var mapView: MapView
     private val MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey"
+
+    private val DEFAULT_ZOOM = 15f
+    private var fusedLocationProviderClient: FusedLocationProviderClient? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -41,6 +54,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             return
         }
         mMap!!.isMyLocationEnabled = true
+        getCurrentLocation()
     }
 
     override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
@@ -52,6 +66,34 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             outState.putBundle(MAP_VIEW_BUNDLE_KEY, mapViewBundle)
         }
         mapView.onSaveInstanceState(mapViewBundle)
+        getCurrentLocation()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getCurrentLocation() {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        try {
+            val location = fusedLocationProviderClient!!.lastLocation
+            location.addOnCompleteListener(object : OnCompleteListener<Location> {
+                override fun onComplete(p0: Task<Location>) {
+                    if (p0.isSuccessful) {
+                        val currentLocation = p0.result as Location?
+                        if (currentLocation != null) {
+                            moveCamera(
+                                LatLng(currentLocation.latitude, currentLocation.longitude),
+                                DEFAULT_ZOOM
+                            )
+                        }
+                    }
+                }
+            })
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun moveCamera(latLng: LatLng, zoom: Float) {
+        mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,zoom))
     }
 
 
